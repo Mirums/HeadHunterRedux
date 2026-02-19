40ec4fbc55aa196fb849cda7cdbdf37d95bf3757
Mirums <morkeyc@gmail.com>
2026-02-19 17:15:39 +0300

    add test

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react'
-import { test, expect } from 'vitest'
+import {render, screen} from '@testing-library/react'
+import {test, expect, vi} from 'vitest'
 import App from '../App'
+import {Provider} from "react-redux";
+import {store} from "../store/store.ts";
+import {MantineProvider} from "@mantine/core";
 
+vi.mock('../store/vacanciesSlice', async () => {
+    const actual = await vi.importActual('../store/vacanciesSlice')
+    return {
+        ...actual,
+        fetchVacancies: () => () => {
+        },
+    }
+})
 test('renders App component', () => {
-    render(<App />)
+    render(
+        <Provider store={store}>
+            <MantineProvider>
+                <App/>
+            </MantineProvider>
+        </Provider>
+    )
     expect(screen.getByText('Список вакансий')).toBeInTheDocument()
 })
+
--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,22 @@
 import '@testing-library/jest-dom'
+Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: (query: string) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: () => {}, // deprecated
+        removeListener: () => {}, // deprecated
+        addEventListener: () => {},
+        removeEventListener: () => {},
+        dispatchEvent: () => false,
+    }),
+})
+class ResizeObserver {
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+}
+
+// @ts-ignore
+global.ResizeObserver = ResizeObserver
